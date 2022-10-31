59c8f5b16d7e14d7f3425a6cffa512d4e02856bf
mehedi432 <aamehedi93@gmail.com>
2022-10-31 20:29:34 +0600

    Adding project1 version

---
+++ 1_project/core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Item, OrderItem, Order
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
---
+++ 1_project/readme.md
@@ -0,0 +1,9 @@
+<div style="text-align: center; font-weight: 'bolder">Hawker</div>
+<hr>
+
+1. A user will be created.
+2. A user will create a shopping cart and make order of items.
+   1. User select an item
+   2. Will go to cart
+   3. If success than an item is ordered
+4. Admin can see the order's
