41359d3e5fc5f2c40534ea97d50f9dd9c5adfa60
Mehedi Abdullah <mehediabdullah@Mehedis-MacBook-Pro.local>
2022-10-31 19:58:56 +0600

    Initial commit

---
+++ project/core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.conf import settings
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Out Wear')
+)
+
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'Secondary'),
+    ('D', 'Danger')
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, default='S')
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2, default='S')
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
